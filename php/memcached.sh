dd822063c6149554381394cada5c314bfb89b4d6
Herbert256 <herbert@groot.jebbink.nl>
2020-09-20 18:44:20 +0200

    no message

--- php/memcached.sh
+++ php/memcached.sh
@@ -5,5 +5,7 @@ apt-get -y install memcached libmemcached-dev libmemcached11 libmemcachedutil2
 pecl channel-update pecl.php.net
 
 printf "\n\n\n\n\n\n\n\n" | pecl install memcached
+printf "\n\n\n\n\n\n\n\n" | pecl install memcached
+printf "\n\n\n\n\n\n\n\n" | pecl install memcached
 
 echo "extension=memcached.so" >> /usr/local/lib/php.ini
